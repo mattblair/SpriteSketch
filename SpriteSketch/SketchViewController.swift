89aa36254a535185505a1d174c4c067a033069f8
Matt Blair <elsewisemedia@gmail.com>
2019-07-17 17:00:21 -0700

    Export multiple sizes at the same time

--- SpriteSketch/SketchViewController.swift
+++ SpriteSketch/SketchViewController.swift
@@ -73,18 +73,21 @@ class SketchViewController: UIViewController, PKCanvasViewDelegate,
     @IBAction func handleExportTapped(_ sender: UIButton) {
         
         // TODO: customize this name by prompting for a name, and adding a timestamp
-        export(rect: canvasView.bounds, filename: UUID().uuidString, scale: UIScreen.main.scale)
+        export(rect: canvasView.bounds,
+               filename: UUID().uuidString,
+               sizeSuffix: "@2x",
+               scale: UIScreen.main.scale)
     }
     
     /// Export the specified rect to a PNG file, at the specified scale.
-    func export(rect: CGRect, filename: String, scale: CGFloat) {
+    func export(rect: CGRect, filename: String, sizeSuffix: String, scale: CGFloat) {
         
         let drawingImage = canvasView.drawing.image(from: rect, scale: scale)
         
         // TODO: Add the option to save elsewhere?
         // TODO: handling for @2x and @3x?
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let filePath = paths.first!.appendingPathComponent("\(filename)@2x.png")
+        let filePath = paths.first!.appendingPathComponent("\(filename)\(sizeSuffix).png")
         
         if let data = drawingImage.pngData() {
             do {
@@ -93,6 +96,25 @@ class SketchViewController: UIViewController, PKCanvasViewDelegate,
                 print("Failed to save: \(error)")
             }
         }
+    }
+    
+    /// Export the rect in all relevant sizes.
+    ///
+    /// If 1.5x of the size of the rect to export is smaller than the size of the selection, this includes an @3x version.
+    /// Assumption: the scale parameter has already been multiplied by screen scale.
+    func exportAllSizes(rect: CGRect, filename: String, scale: CGFloat) {
+        
+        let at3xScale = (scale * 1.5) / UIScreen.main.scale
+        
+        if let oSize = selectedRect?.size,
+            oSize.width >= rect.width * at3xScale,
+            oSize.height >= rect.height * at3xScale {
+            
+            export(rect: rect, filename: filename, sizeSuffix: "@3x", scale: scale * 1.5)
+        }
+        
+        export(rect: rect, filename: filename, sizeSuffix: "@2x", scale: scale)
+        export(rect: rect, filename: filename, sizeSuffix: "@1x", scale: scale / 2.0)
     }
     
     @objc
@@ -185,7 +207,7 @@ class SketchViewController: UIViewController, PKCanvasViewDelegate,
             // Assumption: ExportVC will validate that the aspect ratio is the same,
             // so we only need to check one dimension.
             let exportScale = (exportSize.width / exportRect.width) * UIScreen.main.scale
-            export(rect: exportRect, filename: exportName, scale: exportScale)
+            exportAllSizes(rect: exportRect, filename: exportName, scale: exportScale)
         }
         
         clearExportSelection()
